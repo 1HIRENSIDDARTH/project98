99f017ee6a4ff6e38b4a803465825b8800c6f33c
1HIRENSIDDARTH <92012726+1HIRENSIDDARTH@users.noreply.github.com>
2021-10-08 16:46:34 +0530

    Add files via upload

---
+++ sf.py
@@ -0,0 +1,17 @@
+def SwapFileData():
+    swap1=input('enter file name:')
+    swap2=input('enter file name:')
+
+    with open(swap1,'r') as a:
+        data_a = a.read()
+   
+    with open(swap2,'r') as b:
+        data_b = b.read()
+
+    with open(swap1,'w') as a:
+        a.write(data_b)
+
+    with open(swap2,'w') as b:
+        b.write(data_a)
+   
+SwapFileData()     
